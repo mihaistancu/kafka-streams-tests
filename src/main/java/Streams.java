e1a09ccebf188175744965edfd5b5f54df9f5796
mihaistancu <mihai-dorian.stancu@ext.ec.europa.eu>
2024-09-17 12:16:21 +0200

    Close streams app properly

--- src/main/java/Streams.java
+++ src/main/java/Streams.java
@@ -3,7 +3,9 @@ import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
 
+import java.time.Duration;
 import java.util.*;
+import java.util.concurrent.CountDownLatch;
 
 public class Streams {
     public static void main(String[] args) throws Exception {
@@ -20,9 +22,20 @@ public class Streams {
         properties.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE_V2);
         properties.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
 
+        var latch = new CountDownLatch(1);
+
         try (var streams = new KafkaStreams(topology, properties)) {
+
+            Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+                @Override
+                public void run() {
+                    streams.close(Duration.ofSeconds(5));
+                    latch.countDown();
+                }
+            });
+
             streams.start();
-            System.in.read();
+            latch.await();
         }
     }
 }
